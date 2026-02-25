495d624261aa74bbc86882bae2b12e1e742743cd
Patrick <patrick_schauer@t-online.de>
2026-02-25 16:38:50 +0100

    feat: TasksDB available in AddTask

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TasksDb } from '../../core/db/tasks.db';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-
   imports: [],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
 export class AddTask {
-
+  tasksDb = inject(TasksDb);
 }
