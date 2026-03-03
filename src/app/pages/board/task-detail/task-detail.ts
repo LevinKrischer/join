d604ed74c7c341c898e7f507147823792aa67f2b
Patrick <patrick_schauer@t-online.de>
2026-03-03 16:34:35 +0100

    feat: task-detail: make delete btn work with closing modal

--- src/app/pages/board/task-detail/task-detail.ts
+++ src/app/pages/board/task-detail/task-detail.ts
@@ -1,6 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Task } from '../../../core/db/tasks.db';
+import { TasksDb, Task } from '../../../core/db/tasks.db';
 
 @Component({
     selector: 'app-task-detail',
@@ -10,8 +10,10 @@ import { Task } from '../../../core/db/tasks.db';
     styleUrls: ['./task-detail.scss'],
 })
 export class TaskDetailComponent {
-    task = input.required<Task>();
+    task = input.required<Task>(); // Wired in board.html
     close = output<void>();
+
+    private taskDbSingleton = inject(TasksDb);
 
     /** returns the appropriate priority icon path for the current task */
     get priorityIcon(): string {
@@ -52,5 +54,16 @@ export class TaskDetailComponent {
             .join('')
             .slice(0, 2)
             .toUpperCase();
+    }
+
+    async deleteTask() {
+      const taskId = this.task().id;
+      await this.taskDbSingleton.deleteTask(taskId);
+
+      this.close.emit();
+    }
+
+    async updateTask() {
+      console.log("Hier fehlt noch die Update-Logik!");
     }
 }
