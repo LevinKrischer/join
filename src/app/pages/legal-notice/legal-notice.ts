ea72046265d30468be5b69998df200184a1f0e97
LevinKrischer <levin@levinkrischer.de>
2026-03-09 17:50:16 +0100

    ui: add back-button to legal notice and privacy policy pages

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BackButton } from '../../shared/ui/forms/back-button/back-button';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [BackButton],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
